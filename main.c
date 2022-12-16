eec35232d633a5d2406af34614ec917d61ef6e95
Mateoswiatek <mateoswiatek@student.agh.edu.pl>
2022-12-16 01:13:57 +0100

    Unie działają

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
-union unia1{
-    int w_int;
-    double pole2;
-    char pole3[16];
-} ;
+union unia{
+    int szukana_ocena;
+    char szukane_nazwisko[100];
+};
 int main(void){
-    union unia1 u1, u2;
-    u1.w_int = 2;
-    u1.pole2 = 4.5;
-    //printf("%d\n",  sizeof(u1));
-    printf("%d %lf %s\n", u1.w_int, u1.pole2);
+    union unia u1;
+    u1.szukana_ocena = 2;
+    printf("%d\n",  sizeof(u1));
+    printf("ocenka %d leci do %s\n", u1.szukana_ocena, u1.szukane_nazwisko);
+    strcpy(u1.szukane_nazwisko , "Swiatek");
+    printf("ocenka %d leci do %s\n", u1.szukana_ocena, u1.szukane_nazwisko);
     return 0;
 }
